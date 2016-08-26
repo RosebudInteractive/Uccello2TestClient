2e66dfaf11374a2a87a94000c36de3118fbfa0bf
SashaSokolov <alexander.f.sokolov@gmail.com>
2016-08-26 11:17:05 +0300

    Initial commit.

---
+++ multi_test.js
@@ -0,0 +1,131 @@
+﻿var child_process = require('child_process');
+var util = require('util');
+var fs = require('fs');
+
+var uccelloDir = 'Uccello';
+var UccelloConfig = require('../' + uccelloDir + '/config/config');
+UCCELLO_CONFIG = new UccelloConfig();
+var logger = require('../' + uccelloDir + '/system/winstonLogger');
+
+var NUM_INSTANCES = 20;
+
+var URL = "127.0.0.1";
+var USER = "u1"
+var PWD = "p1";
+var TESTNUM = 1;
+
+var testModule = "./test.js";
+var child_processes = {};
+var nproc = 0;
+
+var _url = URL;
+var _user = USER
+var _pwd = PWD;
+var _num_proc = NUM_INSTANCES;
+var _test_num = TESTNUM;
+var _log_to_files = false;
+var _ch_trace_flag = false;
+
+for (var _cnt = 0; _cnt < process.argv.length; _cnt++) {
+    var _arg = process.argv[_cnt];
+    if (_arg.indexOf("-a") == 0) {
+        _url = _arg.substring(2);
+        console.log("Server address: " + _url);
+    } else
+        if (_arg.indexOf("-u") == 0) {
+            _user = _arg.substring(2);
+            console.log("USER: " + _user);
+        }
+        else
+            if (_arg.indexOf("-p") == 0) {
+                _pwd = _arg.substring(2);
+                console.log("Password: " + _pwd);
+            }
+            else
+                if (_arg.indexOf("-n") == 0) {
+                    _num_proc = _arg.substring(2);
+                    console.log("Child processes: " + _num_proc);
+                }
+                else
+                    if (_arg.indexOf("-t") == 0) {
+                        _test_num = Number(_arg.substring(2));
+                        console.log("Test: #" + _test_num);
+                    }
+                    else
+                        if (_arg.indexOf("-l") == 0) {
+                            _log_to_files = true;
+                            console.log("Log to files.");
+                        }
+                        else
+                            if (_arg === "-chTrace") {
+                                _ch_trace_flag = true;
+                                console.log("Low-level channel tracing is enabled.");
+                            }
+};
+
+function onChildError(pId) {
+    return function (err) {
+        logger.error("Erorr [" + pId + "]: " + JSON.stringify(err));
+    };
+};
+
+function onChildExit(pId) {
+    return function (code, signal) {
+        logger.info("Exit [" + pId + "] code: " + code + ", signal: " + signal);
+    };
+};
+
+function onChildStdOut(pId) {
+    return function (data) {
+        var str = String(data);
+        logger.info("[" + pId + "]: " + str.substring(0, str.length - 1));
+    };
+};
+
+function onChildStdErr(pId) {
+    return function (data) {
+        var str = String(data);
+        logger.error("[" + pId + "]: " + str.substring(0, str.length - 1));
+        //logger.error("STOP ON ERROR!!!");
+        //process.exit(1);
+        //throw new Error("STOP ON ERROR!!!");
+    };
+};
+
+for (var i = 0; i < _num_proc; i++) {
+    var opts = {
+    //stdio: [process.stdin, process.stdout, process.stderr],
+    cwd: "./"
+    };
+    
+    var params = [
+        testModule,
+        "-a" + _url,
+        "-u" + _user,
+        "-p" + _pwd,
+        "-t" + _test_num
+    ];
+    
+    if (_ch_trace_flag)
+        params.push("-chTrace");
+
+    var child = child_process.spawn("node", params, opts);
+    nproc++;
+    
+    var strPid = String(child.pid);
+    var fstream = null;
+    if (_log_to_files)
+        fstream = fs.createWriteStream("log_" + strPid + ".log");
+
+    child_processes[strPid] = { child: child, err: null, fstream: fstream };
+    child.on("error", onChildError(strPid));
+    child.on("exit", onChildExit(strPid));
+    
+    if (_log_to_files) {
+        child.stdout.pipe(fstream);
+        child.stderr.pipe(fstream);
+    } else
+        child.stdout.on("data", onChildStdOut(strPid));
+
+    child.stderr.on("data", onChildStdErr(strPid));
+};
---
+++ test.js
@@ -0,0 +1,289 @@
+﻿// дирректория где лежит Uccello
+//var uccelloDir = process.argv[2]?process.argv[2]:'Uccello';
+var uccelloDir = 'Uccello';
+console.log('Using folder: ' + uccelloDir);
+
+// GLOBAL DEFINITIONS (simulate browser environment)
+//
+////////////////////////////////////////////////////
+global.$ = { cookie: function () { } };
+global.url = function () {
+    return _url;
+};
+global.navigator = { userAgent: "test" };
+global.io = require('socket.io-client');
+////////////////////////////////////////////////////
+
+global.performance = { now: function () { return Date.now(); } }
+
+//logger = { info: function (msg) { console.log(msg); } };
+var config = {
+    controls: [
+        { className: 'RootTstCompany', component: '../DataControls/rootTstCompany', guid: 'c4d626bf-1639-2d27-16df-da3ec0ee364e' },
+        { className: 'RootTstContact', component: '../DataControls/rootTstContact', guid: 'de984440-10bd-f1fd-2d50-9af312e1cd4f' },
+        { className: 'RootAddress', component: '../DataControls/rootAddress', guid: '07e64ce0-4a6c-978e-077d-8f6810bf9386' },
+        { className: 'RootCompany', component: '../DataControls/rootCompany', guid: '0c2f3ec8-ad4a-c311-a6fa-511609647747' },
+        { className: 'RootContact', component: '../DataControls/rootContact', guid: 'ad17cab2-f41a-36ef-37da-aac967bbe356' },
+        { className: 'RootContract', component: '../DataControls/rootContract', guid: '4f7d9441-8fcc-ba71-2a1d-39c1a284fc9b' },
+        { className: 'RootIncomeplan', component: '../DataControls/rootIncomeplan', guid: '194fbf71-2f84-b763-eb9c-177bf9ac565d' },
+        { className: 'RootLead', component: '../DataControls/rootLead', guid: '31c99003-c0fc-fbe6-55eb-72479c255556' },
+        { className: 'DataTstContact', component: '../DataControls/dataTstContact', guid: '27ce7537-7295-1a45-472c-a422e63035c7' },
+        { className: 'DataContact', component: '../DataControls/dataContact', guid: '73596fd8-6901-2f90-12d7-d1ba12bae8f4' },
+        { className: 'DataContract', component: '../DataControls/dataContract', guid: '08a0fad1-d788-3604-9a16-3544a6f97721' },
+        { className: 'DataTstCompany', component: '../DataControls/dataTstCompany', guid: '34c6f03d-f6ba-2203-b32b-c7d54cd0185a' },
+        { className: 'DataCompany', component: '../DataControls/dataCompany', guid: '59583572-20fa-1f58-8d3f-5114af0f2c51' },
+        { className: 'DataAddress', component: '../DataControls/dataAddress', guid: '16ec0891-1144-4577-f437-f98699464948' },
+        { className: 'DataLead', component: '../DataControls/dataLead', guid: '86c611ee-ed58-10be-66f0-dfbb60ab8907' },
+        { className: 'DataIncomeplan', component: '../DataControls/dataIncomeplan', guid: '56cc264c-5489-d367-1783-2673fde2edaf' },
+        { className: 'DbNavigator', component: 'dbNavigator', viewset: true, guid: '38aec981-30ae-ec1d-8f8f-5004958b4cfa' },
+        { className: 'MatrixGrid', component: 'matrixGrid', viewset: true, guid: '827a5cb3-e934-e28c-ec11-689be18dae97' },
+        { className: 'PropEditor', component: 'propEditor', viewset: true, guid: 'a0e02c45-1600-6258-b17a-30a56301d7f1' },
+        { className: 'Container', viewset: true },
+        { className: 'CContainer', viewset: true },
+        { className: 'HContainer', viewset: true },
+        { className: 'VContainer', viewset: true },
+        { className: 'GContainer', viewset: true },
+        { className: 'FContainer', viewset: true },
+        { className: 'Form', viewset: true },
+        { className: 'Button', viewset: true },
+        { className: 'DataGrid', viewset: true },
+        { className: 'DataEdit', viewset: true },
+        { className: 'Edit', viewset: true },
+        { className: 'Label', viewset: true }
+    ],
+    viewSet: { name: 'simpleview', path: '/../ProtoOne/public/ProtoControls/simpleview/' },
+    controlsPath: __dirname + '/../ProtoOne/public/ProtoControls/',
+    dataPath: __dirname + '/../ProtoOne/data/',
+    uccelloPath: __dirname + '/../' + uccelloDir + '/'
+};
+
+// модуль настроек
+var UccelloConfig = require('../' + uccelloDir + '/config/config');
+UCCELLO_CONFIG = new UccelloConfig(config);
+DEBUG = false;
+
+var URL = "127.0.0.1";
+var USER = "u1"
+var PWD = "p1";
+var TESTNUM = 1;
+var opt_load = "fast";
+
+var testFormGuid = "88b9280f-7cce-7739-1e65-a883371cd498";
+
+var _url = URL;
+var _user = USER
+var _pwd = PWD;
+var _test_num = TESTNUM;
+var _ch_trace_flag = false;
+
+function getTestNameAndSetOptions(num) {
+    var tname = "Unknown";
+    switch (num) {
+        case 1:
+            tname = "MoveCursor";
+            break;
+        case 2:
+            opt_load = "fast"
+            tname = "LoadNewRoots+fast";
+            break;
+        case 3:
+            opt_load = "slow"
+            tname = "LoadNewRoots+slow";
+            break;
+    };
+    return tname;
+}
+
+for (var _cnt = 0; _cnt < process.argv.length; _cnt++) {
+    var _arg = process.argv[_cnt];
+    if (_arg.indexOf("-a") == 0) {
+        _url = _arg.substring(2);
+        console.log("Server address: " + _url);
+    } else
+        if (_arg.indexOf("-u") == 0) {
+            _user = _arg.substring(2);
+            console.log("USER: " + _user);
+        }
+        else
+            if (_arg.indexOf("-p") == 0) {
+                _pwd = _arg.substring(2);
+                console.log("Password: " + _pwd);
+            }
+            else
+                if (_arg.indexOf("-t") == 0) {
+                    _test_num = Number(_arg.substring(2));
+                    console.log("Test: " + getTestNameAndSetOptions(_test_num));
+                }
+                else
+                    if (_arg === "-chTrace") {
+                        _ch_trace_flag = true;
+                        console.log("Low-level channel tracing is enabled.");
+                        break;
+                    };
+};
+
+var logger = require('../' + uccelloDir + '/system/winstonLogger');
+
+var UccelloClt = require("../" + uccelloDir + "/uccelloClt");
+var CommunicationClient = require("../" + uccelloDir + "/connection/commClient");
+
+var self = this;
+var visualContext;
+var detailRoot = null;
+var counter = 10
+var currId = 1;
+var detailRootGuid = "b49d39c9-b903-cccd-7d32-b84beb1b76dc";
+
+UCCELLO_CONFIG.webSocketClient = UCCELLO_CONFIG.webSocketClient ? UCCELLO_CONFIG.webSocketClient : {};
+UCCELLO_CONFIG.webSocketClient.io_log_flag = _ch_trace_flag;
+
+var commClient = new CommunicationClient.Client(UCCELLO_CONFIG.webSocketClient);
+var uccelloClt = new UccelloClt( {
+    callback: function () {
+        logger.info("Connected to the SERVER !");
+        var user = uccelloClt.getUser();
+        console.time('applyDeltas');
+        login(_user, _pwd, function (res) {
+            logger.info("Login: " + res);
+            switch (_test_num) {
+                case 1:
+                    testMoveCursor(uccelloClt);
+                    break;
+                case 2:
+                case 3:
+                    testLoadNewRoots(uccelloClt);
+                    break;
+                default:
+                    throw new Error("### Unknown test number: " + _test_num);
+            };
+        });
+    },
+    newTabCallback: function () {
+        console.log("uccelloClt.newTabCallback");
+    },
+    commClient: commClient
+});
+
+function testMoveCursor(uccelloClt) {
+    var visualContext;
+    var detailRoot;
+    uccelloClt.createContext('server', [testFormGuid], function (result) {
+        logger.info("Context was loaded !");
+        visualContext = uccelloClt.getSysCM().getObj(result.vc);
+        uccelloClt.setContext({ vc: result.vc, side: result.side, formGuids: result.roots, urlFormGuids: 'all' }, function (arg) {
+            logger.info("Context was set !");
+            if (!result.roots[0])
+                throw new Error("Invalid result.roots[0]: " + JSON.stringify(result.roots[0]));
+            internalTest();
+            function internalTest() {
+                var mainForm = visualContext.getContentDB().getObj(result.roots[0]);
+                if (!mainForm) {
+                    //throw new Error("Missing object \"mainForm\": " + JSON.stringify(result.roots[0]));
+                    var msg = "Missing object \"mainForm\": " + JSON.stringify(result.roots[0]);
+                    logger.error(msg);
+                    logger.info(msg);
+                    setTimeout(internalTest, 10000);
+                } else {
+                    var datasetCompany = visualContext.getContentDB().getObjByRoot(mainForm, "061b1347-18a2-a829-fcce-ba59058b4bdf");
+                    var datasetAddress = visualContext.getContentDB().getObjByRoot(mainForm, "92720532-a58f-68bb-6e5a-02b28239e312");
+                    visualContext.loadNewRoots([datasetCompany.getSerialized("ObjType")], { rtype: "data" }, function (res) {
+                        visualContext.loadNewRoots([datasetAddress.getSerialized("ObjType")], { rtype: "data", expr: 1 }, function (res) {
+                            logger.info("New root was loaded !");
+                            var curr_item = 0;
+                            var dbVer;
+                            function moveCursor(res) {
+                                var cursor = datasetAddress.cursor();
+                                if (typeof cursor == "number") {
+                                    var colAddress = datasetAddress.root().getCol("DataElements");
+                                    totOp++;
+                                    
+                                    var newVer = (res && res.delta) ? res.delta.dbVersion : ((res && res.data) ? res.data.dbVersion : null);
+
+                                    if (dbVer && res && ((newVer - dbVer) != 1)) {
+                                        console.log("### WARNING: prev version: " + dbVer + ", next: " + newVer + ".");
+                                    };
+                                    dbVer = visualContext.getContentDB().getVersion("valid");
+                                    curr_item++;
+                                    if (curr_item >= colAddress.count())
+                                        curr_item = 0;
+                                    datasetAddress.cursor(colAddress.get(curr_item).id());
+                                    visualContext.getContentDB().getController().genDeltas(visualContext.getContentDB().getGuid(), moveCursor);
+                                    //visualContext.getContentDB().getController().genDeltas(visualContext.getContentDB().getGuid(), true, moveCursor);
+                                    //console.log("Cursor: " + cursor);
+                                } else
+                                    setTimeout(moveCursor, 0);
+                            };
+                            moveCursor();
+                        });
+                    });
+                };
+            };
+        }, function () {
+            //logger.info("Render root.");
+        });
+    });
+};
+
+function testLoadNewRoots(uccelloClt) {
+    var visualContext;
+    var detailRoot;
+    var isFirstTime = true;
+
+    uccelloClt.createContext('server', [testFormGuid], function (result) {
+        logger.info("Context loaded !");
+        visualContext = uccelloClt.getSysCM().getObj(result.vc);
+        uccelloClt.setContext({ vc: result.vc, side: result.side, formGuids: result.roots, urlFormGuids: 'all' }, function (arg) {
+            visualContext.loadNewRoots([detailRootGuid], { rtype: "data", expr: currId++ }, function loadDetail(res) {
+                if (isFirstTime) {
+                    logger.info("New root loaded !");
+                    isFirstTime = false;
+                };
+                totOp++;
+                if ((opt_load === "fast") && (res.guids[0]))
+                    detailRoot = visualContext.getContentDB().getObj(res.guids[0]);
+                if (currId > counter)
+                    currId = 1;
+                if (opt_load === "fast")
+                    visualContext.loadNewRoots([detailRoot.getGuid()], { rtype: "data", expr: currId++ }, loadDetail);
+                else
+                    visualContext.loadNewRoots([detailRootGuid], { rtype: "data", expr: currId++ }, loadDetail);
+            });
+        }, function () {
+            //logger.info("Render root.");
+        });
+    });
+};
+
+function login (name, pass, done) {
+    var session = { guid: uccelloClt.getSessionGuid(), deviceName: 'MyComputer', deviceType: 'C', deviceColor: '#6ca9f0' };
+    uccelloClt.getClient().authenticate({ user: name, pass: pass, session: session }, function (result) {
+        if (result.user) {
+            uccelloClt.subscribeUser(function (result2) {
+                if (!result2) {
+                    logger.error("subscribeUser failed.");
+                } else {
+                    logger.info("User has been subscribed !");
+                }
+                if (done)
+                    done(true);
+            });
+        } else {
+            logger.error("Login failed.");
+            if (done)
+                done(false);
+        }
+    });
+};
+
+var _timeout = 60000;
+var totOp = 0;
+var start = new Date();
+
+setTimeout(function showDbs() {
+    var end = new Date();
+    console.log("### [" + getTestNameAndSetOptions(_test_num) + "] Total ops: " + totOp + ", time: " + (end - start) + " ms, rate: " 
+        + (totOp * 1000 / (end - start)).toFixed(2) + " op/sec.");
+    start = end;
+    totOp = 0;
+    setTimeout(showDbs, _timeout);
+}, _timeout);
